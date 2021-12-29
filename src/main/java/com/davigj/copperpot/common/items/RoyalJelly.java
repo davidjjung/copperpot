8ade247a665a4de7e1dcc4c555497e631346961a
davidjjung <david.j.jung@gmail.com>
2021-12-28 20:36:27 -0500

    royal jelly has laughed its last laugh.

--- src/main/java/com/davigj/copperpot/common/items/RoyalJelly.java
+++ src/main/java/com/davigj/copperpot/common/items/RoyalJelly.java
@@ -1,7 +1,9 @@
 package com.davigj.copperpot.common.items;
 
 import com.davigj.copperpot.core.registry.CopperPotItems;
+import com.davigj.copperpot.core.utils.TextUtils;
 import com.google.common.collect.Lists;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.BeeEntity;
@@ -12,8 +14,10 @@ import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.EffectUtils;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.*;
+import net.minecraft.util.text.*;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,6 +26,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 import vectorwing.farmersdelight.utils.MathUtils;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Supplier;
@@ -31,6 +36,32 @@ public class RoyalJelly extends Item {
 
     public RoyalJelly(Item.Properties properties) {
         super(properties);
+    }
+
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        IFormattableTextComponent textWhenFeeding = TextUtils.getTranslation("tooltip.royal_jelly.when_feeding", new Object[0]);
+        IFormattableTextComponent textWhenEating = TextUtils.getTranslation("tooltip.royal_jelly.when_eating", new Object[0]);
+        tooltip.add(textWhenEating.mergeStyle(TextFormatting.BLUE));
+        tooltip.add(textWhenFeeding.mergeStyle(TextFormatting.GRAY));
+
+        StringTextComponent effectDescription;
+        Effect effect;
+        for (Iterator var6 = EFFECTS.iterator(); var6.hasNext(); tooltip.add(effectDescription.mergeStyle(effect.getEffectType().getColor()))) {
+            EffectInstance effectInstance = (EffectInstance) var6.next();
+            effectDescription = new StringTextComponent(" ");
+            IFormattableTextComponent effectName = new TranslationTextComponent(effectInstance.getEffectName());
+            effectDescription.append(effectName);
+            effect = effectInstance.getPotion();
+            if (effectInstance.getAmplifier() > 0) {
+                effectDescription.appendString(" ").append(new TranslationTextComponent("potion.potency." + effectInstance.getAmplifier()));
+            }
+
+            if (effectInstance.getDuration() > 20) {
+                effectDescription.appendString(" (").appendString(EffectUtils.getPotionDurationString(effectInstance, 1.0F)).appendString(")");
+            }
+        }
+        IFormattableTextComponent textWhenBeeing = TextUtils.getTranslation("tooltip.royal_jelly.fix_bee", new Object[0]);
+        tooltip.add(textWhenBeeing.mergeStyle(TextFormatting.BLUE));
     }
 
     @Override
@@ -65,7 +96,9 @@ public class RoyalJelly extends Item {
     }
 
     static {
-        EFFECTS = Lists.newArrayList(new EffectInstance[]{new EffectInstance(Effects.ABSORPTION, 1200, 1), new EffectInstance(Effects.RESISTANCE, 1200, 1)});
+        EFFECTS = Lists.newArrayList(new EffectInstance(Effects.ABSORPTION, 1200, 1),
+                new EffectInstance(Effects.RESISTANCE, 1200, 1),
+                new EffectInstance(Effects.SPEED, 1200, 0));
     }
 
     private static Supplier<Effect> getCompatEffect(String modid, ResourceLocation effect) {
@@ -94,11 +127,11 @@ public class RoyalJelly extends Item {
                         entity.ageUp(120, true);
                     }
 
-                    beeFx(ModList.get().isLoaded("buzzier_bees"), entity);
+                    beeFx(entity);
 
                     entity.setAngerTime(0);
 
-                    entity.world.playSound((PlayerEntity) null, target.getPosition(), SoundEvents.ENTITY_GENERIC_EAT, SoundCategory.PLAYERS, 0.5F, 1.3F);
+                    entity.world.playSound((PlayerEntity) null, target.getPosition(), SoundEvents.ENTITY_GENERIC_EAT, SoundCategory.PLAYERS, 0.5F, 1.4F);
 
                     for (int i = 0; i < 5; ++i) {
                         double d0 = MathUtils.RAND.nextGaussian() * 0.02D;
@@ -117,17 +150,14 @@ public class RoyalJelly extends Item {
             }
         }
 
-        private static void beeFx(boolean buzzLoaded, BeeEntity entity) {
+        private static void beeFx(BeeEntity entity) {
             List<EffectInstance> list = RoyalJelly.EFFECTS;
-            if (buzzLoaded) {
-                list.add(new EffectInstance(new EffectInstance(getCompatEffect("buzzier_bees", new ResourceLocation(
-                        "buzzier_bees", "sunny")).get(), 12000, 0)));
-            }
             Iterator<EffectInstance> var7 = list.iterator();
             while (var7.hasNext()) {
                 EffectInstance effect = (EffectInstance) var7.next();
                 entity.addPotionEffect(new EffectInstance(effect));
             }
         }
+
     }
 }
