df6cf5cb90ee5c2b659cbeece2ba54eb9fbe8016
davidjjung <david.j.jung@gmail.com>
2022-01-31 10:21:57 -0500

    hopefully reducing redundant .bak files. thanks bagel

--- src/main/java/com/davigj/copperpot/core/CopperPotConfig.java
+++ src/main/java/com/davigj/copperpot/core/CopperPotConfig.java
@@ -1,12 +1,10 @@
 package com.davigj.copperpot.core;
 
+import com.google.common.collect.Lists;
 import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 
 public class CopperPotConfig {
 
@@ -26,28 +24,27 @@ public class CopperPotConfig {
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> aestivalExtensionFx;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> brumalExtensionFx;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> vernalExtensionFx;
-        public final ForgeConfigSpec.ConfigValue<Double> copperFumeRadius;
-
+        public final ForgeConfigSpec.DoubleValue copperFumeRadius;
 
         // define
-        public Common(ForgeConfigSpec.Builder builder) {
+        Common(ForgeConfigSpec.Builder builder) {
             mooncakeBadReactDims = builder.comment("A list of dimensions in which mooncakes apply adverse effects " +
                     "to the player when consumed, usually due to the lack of a moon.").define("mooncakeBadReactDims",
-                    new ArrayList<>(Arrays.asList("minecraft:the_nether", "minecraft:the_end")), s -> s instanceof String);
+                    Lists.newArrayList("minecraft:the_nether", "minecraft:the_end"));
             autumnalExtensionFx = builder.comment("A list of effects which will be extended upon consumption of autumnal agar.")
-                    .define("autumnalExtensionFx", new ArrayList<>(Arrays.asList("effect.minecraft.resistance",
-                            "effect.farmersdelight.nourished", "effect.minecraft.absorption", "effect.atmospheric.persistence")), s -> s instanceof String);
+                    .define("autumnalExtensionFx", Lists.newArrayList("effect.minecraft.resistance",
+                            "effect.farmersdelight.nourished", "effect.minecraft.absorption", "effect.atmospheric.persistence"));
             aestivalExtensionFx = builder.comment("A list of effects which will be extended upon consumption of aestival agar.")
-                    .define("aestivalExtensionFx", new ArrayList<>(Arrays.asList("effect.minecraft.night_vision",
-                            "effect.atmospheric.relief", "effect.minecraft.water_breathing", "effect.minecraft.strength")), s -> s instanceof String);
+                    .define("aestivalExtensionFx", Lists.newArrayList("effect.minecraft.night_vision",
+                            "effect.atmospheric.relief", "effect.minecraft.water_breathing", "effect.minecraft.strength"));
             brumalExtensionFx = builder.comment("A list of effects which will be extended upon consumption of brumal agar.")
-                    .define("brumalExtensionFx", new ArrayList<>(Arrays.asList("effect.farmersdelight.comfort",
-                            "effect.minecraft.invisibility", "effect.minecraft.fire_resistance", "effect.minecraft.slow_falling")), s -> s instanceof String);
+                    .define("brumalExtensionFx", Lists.newArrayList("effect.farmersdelight.comfort",
+                            "effect.minecraft.invisibility", "effect.minecraft.fire_resistance", "effect.minecraft.slow_falling"));
             vernalExtensionFx = builder.comment("A list of effects which will be extended upon consumption of vernal agar.")
-                    .define("vernalExtensionFx", new ArrayList<>(Arrays.asList("effect.minecraft.regeneration",
-                            "effect.minecraft.jump_boost", "effect.minecraft.haste", "effect.upgrade_aquatic.vibing")), s -> s instanceof String);
+                    .define("vernalExtensionFx", Lists.newArrayList("effect.minecraft.regeneration",
+                            "effect.minecraft.jump_boost", "effect.minecraft.haste", "effect.upgrade_aquatic.vibing"));
             copperFumeRadius = builder.comment("The horizontal radius for which copper pots will grant effects with no fume inhibitors involved.")
-                    .define("copperFumeRadius", 3.0D);
+                    .defineInRange("copperFumeRadius", 3.0D, 1.0D, Double.MAX_VALUE);
         }
     }
 }
