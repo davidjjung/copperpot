d6a0a6e95a7380f08f4566a4c0e9745662a0151e
DavigJ <david.j.jung@gmail.com>
2024-07-14 17:21:03 +0400

    fix meringue block collision

--- src/main/java/com/davigj/copperpot/common/block/MeringueBlock.java
+++ src/main/java/com/davigj/copperpot/common/block/MeringueBlock.java
@@ -2,8 +2,6 @@ package com.davigj.copperpot.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -16,18 +14,16 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.joml.Vector3d;
 
 public class MeringueBlock extends Block {
-    protected static final VoxelShape SHAPES = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D);
+    protected static final VoxelShape SHAPE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D);
 
     public MeringueBlock(Properties properties) {
         super(properties);
     }
 
-    @Override
-    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPES;
+    public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
     }
 
     @Override
@@ -63,34 +59,6 @@ public class MeringueBlock extends Block {
                 }
             }
             if (living.jumping && vector3d.y > 0) {
-//                // we know you're ascending. you're ascending at a speed bounded by
-//                // 0.3 + (amp*0.1) or 0
-////                entity.setMotion(vector3d.x, Math.max(0.3 + (amp * 0.1D), vector3d.y+ (amp * 0.1D)), vector3d.z);
-//
-//                if (((LivingEntity) entity).isJumping && vector3d.y > 0) {
-//                    // we know you're ascending. you're ascending at a speed bounded by
-//                    // 0.3 + (amp*0.1) or 0. If amp is positive, 0.3 + (amp*0.1) is the top bound.
-//                    // If amp is negative, 0 is the bottom bound. You maintain your normal v3y speed
-//                    // if your calculation exceeds the top bound, and stop if it looks to exceed the bottom bound.
-//                    // In other words, amp * 0.1 cannot go below -0.3.
-//                    if (vector3d.y < 0.3 + (amp * 0.1D) && amp > 0) {
-//                        entity.setMotion(new Vector3d(vector3d.x, vector3d.y + (amp * 0.1D), vector3d.z));
-//                    } else if (vector3d.y < 0.3 + (amp * 0.1D) && amp <= 0) {
-//                        entity.setMotion(new Vector3d(vector3d.x, Math.max(0.03, vector3d.y + (amp * 0.1D)), vector3d.z));
-//                    }
-//                    if (rand > 0.99) {
-//                        entity.playSound(SoundEvents.BLOCK_HONEY_BLOCK_STEP, 0.4F, 1.1F);
-//                    }
-//                } else if (vector3d.y < 0D) {
-//                    if(amp < 0) {
-//                        entity.setMotion(new Vector3d(vector3d.x, Math.min(-0.03, Math.max(-0.1D, -0.1D - (amp * 0.05D))), vector3d.z));
-//                    } else {
-//                        entity.setMotion(new Vector3d(vector3d.x, -0.1D, vector3d.z));
-//                    }
-//                    if (rand > 0.99) {
-//                        entity.playSound(SoundEvents.BLOCK_HONEY_BLOCK_STEP, 0.4F, 1.1F);
-//                    }
-//                }
                 if (Math.abs(vector3d.y) < 0.3 + (amp * 0.1D)) {
                     entity.setDeltaMovement(new Vec3(vector3d.x, Math.max(vector3d.y, vector3d.y + ((amp + 1) * 0.1D)), vector3d.z));
                 } else {
